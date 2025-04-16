6050e0190fb055ff50b280a21959a4d1531eedd1
Segundavid-Dev <segdavid03@gmail.com>
2025-04-16 22:37:05 +0100

    started working on hover button functioality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,12 @@
 export default function Button() {
+  function handleOnMouse() {
+    console.log("Mouse is over the button");
+  }
   return (
-    <div className="bg-white p-3 rounded-full border border-[var(--product-category-color)] w-40 absolute bottom-15 right-6">
+    <div
+      className="bg-white p-3 rounded-full border border-[var(--product-category-color)] w-40 absolute bottom-25 right-6 hover:bg-[var(--cart-red)] hover:text-white duration-300"
+      onMouseEnter={handleOnMouse}
+    >
       <div className="flex items-center justify-center gap-2 cursor-pointer">
         <img src="/images/icon-add-to-cart.svg" alt="" />
         <small className="font-bold">Add to Cart</small>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import Button from "./button";
+
 export default function Card({ item }) {
-  console.log(item);
   return (
     <div>
       <div className="relative">
@@ -13,13 +13,15 @@ export default function Card({ item }) {
         </div>
         <Button />
 
-        <small className="text-[var(--product-category-color)] font-semibold">
-          {item.category}
-        </small>
-        <p className="font-bold">{item?.name}</p>
-        <small className="text-[var(--cart-red)] font-bold">
-          ${item?.price}
-        </small>
+        <div className="pt-10">
+          <small className="text-[var(--product-category-color)] font-semibold">
+            {item.category}
+          </small>
+          <p className="font-bold">{item?.name}</p>
+          <small className="text-[var(--cart-red)] font-bold">
+            ${item?.price}
+          </small>
+        </div>
       </div>
     </div>
   );
