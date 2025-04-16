74a8e34d2c240684b796916b9b6e96a126eae274
Segundavid-Dev <segdavid03@gmail.com>
2025-04-16 15:13:04 +0100

    rendered fetched items on the UI

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
+
 export default function Product() {
+  // create a state array to hold all fetch values
+  const [data, setData] = useState([]);
+  useEffect(function () {
+    // create an async function to fetch json data
+    async function fetchData() {
+      const response = await fetch("/data.json");
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData();
+  }, []);
   return (
     <div>
-      <h1 className="text-3xl font-extrabold">Desserts</h1>
+      <h1 className="text-4xl font-extrabold">Desserts</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.name}>
+            <Card />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
