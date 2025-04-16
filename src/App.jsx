fcacbc3202365e5c4027f1bce6357d3414bf0c61
Segundavid-Dev <segdavid03@gmail.com>
2025-04-16 13:22:56 +0100

    Implemented component composition for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-export default function App() {
+import Cart from "./components/Cart";
+import Product from "./components/Product";
+
+function App() {
   return (
-    <div>
-      <h1>Project Setup</h1>
+    <div className="container mx-auto p-10">
+      <div className="flex justify-center">
+        <Product />
+        <Cart />
+      </div>
     </div>
   );
 }
+
+export default App;
+
+/*
+ 2 main components
+  * Cart components
+  * Product components
+    * Cards components
+      *card components   
+*/
