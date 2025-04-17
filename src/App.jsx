74eb7353c0ad6984e255d7c6439cac4f7c5f1c2f
Segundavid-Dev <segdavid03@gmail.com>
2025-04-17 12:46:26 +0100

    consumed context value

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Cart from "./components/Cart";
 import Product from "./components/Product";
+import CounterProvider from "./context/CounterProvider";
 
 function App() {
   return (
-    <div className="container mx-auto p-10">
-      <div className="flex justify-between">
-        <Product />
-        <Cart />
+    <CounterProvider>
+      <div className="container mx-auto p-10">
+        <div className="flex justify-between">
+          <Product />
+          <Cart />
+        </div>
       </div>
-    </div>
+    </CounterProvider>
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,48 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { CounterContext } from "../context/CounterContext";
 
 export default function Button() {
   const [isHover, setIsHover] = useState(false);
+  const { count, setCount } = useContext(CounterContext);
 
-  function handleOnClick() {
+  // Desktop hover functionality
+  function handleOnMouseEnter() {
     setIsHover(!isHover);
   }
+
+  function handleOnMouseLeave() {
+    setIsHover(false);
+  }
+
+  // Mobile click functionality
+  function handleOnTouchStart() {
+    setIsHover(true);
+  }
+
+  function handleOnTouchEnd() {
+    setIsHover(false);
+  }
+
+  function handleAddtoCart() {
+    const newValue = count + 1;
+    setCount(newValue);
+    console.log(newValue);
+  }
+
+  function handleRemoveFromCart() {
+    const newValue = count - 1;
+    setCount(newValue);
+    console.log(newValue);
+  }
+
   return (
-    <div onClick={handleOnClick} className="select-none">
+    <div
+      onMouseEnter={handleOnMouseEnter}
+      onMouseLeave={handleOnMouseLeave}
+      onTouchEnd={handleOnTouchEnd}
+      onTouchStart={handleOnTouchStart}
+      className="select-none"
+    >
       <div
         className={`rounded-full p-3  border border-[var(--product-category-color)] w-40 absolute bottom-25 right-6 cursor-pointer ${
           isHover ? "bg-[var(--cart-red)] text-white border-none" : "bg-white"
@@ -16,13 +51,18 @@ export default function Button() {
         {isHover ? (
           <div className="flex items-center justify-around cursor-pointer">
             <div className="border border-white py-2 px-1 rounded-full">
-              <img src="/images/icon-decrement-quantity.svg" alt="" />
+              <img
+                src="/images/icon-decrement-quantity.svg"
+                alt=""
+                onClick={handleRemoveFromCart}
+              />
             </div>
             <small className="font-bold">1</small>
             <div className="border border-white p-1 rounded-full ">
               <img
                 src="/images/icon-increment-quantity.svg"
                 alt="increment-cart-icon"
+                onClick={handleAddtoCart}
               />
             </div>
           </div>
