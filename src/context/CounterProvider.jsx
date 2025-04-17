b65909139551f09b8ae6136fc529212b65e91efa
Segundavid-Dev <segdavid03@gmail.com>
2025-04-17 13:11:03 +0100

    working on state management

--- src/context/CounterProvider.jsx
+++ src/context/CounterProvider.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { CounterContext } from "./CounterContext";
 
 export default function CounterProvider({ children }) {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState([]);
   return (
     <div>
       <CounterContext.Provider value={{ count, setCount }}>
