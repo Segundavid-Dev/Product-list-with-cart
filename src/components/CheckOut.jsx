d4c84b718da09a9db2d07179a665f8181ee7c8d8
Segundavid-Dev <segdavid03@gmail.com>
2025-04-22 20:40:42 +0100

    create modal component

---
+++ src/components/CheckOut.jsx
@@ -0,0 +1,10 @@
+export default function CheckOut() {
+  return (
+    <div>
+      <h1>Checkout Order Modal</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur, sit!
+      </p>
+    </div>
+  );
+}
